6893fa4036f8af60857dc2a6aedd9f12e28c374a
YaphetKG <kebedey@renci.org>
2021-04-13 00:18:21 -0400

    testing out airflow variables,

--- annotate.py
+++ annotate.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from airflow.operators.bash_operator import BashOperator
-from airflow.models import DAG
+from airflow.models import DAG, Variable
 from dug_helpers.dug_utils import DugUtil
 from roger.core import Util
 from dag_util import default_args, create_python_task, get_config
@@ -21,23 +21,38 @@ with DAG(
                          dag=dag)
     make_kg_tagged = create_python_task(dag, "create_kgx_files", DugUtil.make_kg_tagged)
 
-    # Unzip and get files
-    DugUtil.extract_dbgap_zip_files(config=get_config())
-    DugUtil.get_topmed_files(config=get_config())
-    chunk_size = 10
-    dd_xml_files = Util.dug_dd_xml_objects()
-    topmed_files = Util.dug_topmed_objects()
-    dd_xml_chuncked = [dd_xml_files[start:start + chunk_size] for start in range(0, len(dd_xml_files), chunk_size) ]
-    for index, chunk in enumerate(dd_xml_chuncked):
-        task = create_python_task(dag, f"annotate_and_normalize-DB_GAP-{index}", DugUtil.annotate_db_gap_files,{"files": chunk})
-        intro >> task >> make_kg_tagged
-    topmed_task = create_python_task(dag, f"annotate_and_normalize-TOPMED", DugUtil.annotate_topmed_files, {"files": topmed_files})
-    intro >> topmed_task >> make_kg_tagged
-    # get_topmed_files = create_python_task(dag, "get_topmed_data", DugUtil.get_topmed_files)
-    # extract_db_files = create_python_task(dag, "get_dbgab_data", DugUtil.extract_dbgap_zip_files)
+    # Unzip and get files, avoid this because
+    # 1. it takes a bit of time making the dag itself, webserver hangs
+    # 2. Every task in this dag would still need to execute this part
+    # making it redundant
+    # 3. tasks like intro would fail because they don't have the data dir mounted.
 
-    # dug_load_topmed_variables = create_python_task(dag, "annotate_and_normalize", DugUtil.load_and_annotate)
+    get_topmed_files = create_python_task(dag, "get_topmed_data", DugUtil.get_topmed_files)
+    extract_db_gap_files = create_python_task(dag, "get_dbgab_data", DugUtil.extract_dbgap_zip_files)
 
-    # intro >> [get_topmed_files, extract_db_files] >> \
-    # dug_load_topmed_variables >> make_kg_tagged
+    def setup_db_gab_tasks(**kwargs):
+        db_gap_files = Util.dug_dd_xml_objects()
+        chunk_size = 10
+        chucked = [db_gap_files[start: start + chunk_size] for start in range(0, len(db_gap_files), chunk_size)]
+        Variable.set("db_gap_chunked", chucked)
+
+    db_gap_file_chunks = Variable.get("db_gap_chunked")
+
+    db_gap_task_set = []
+    for index, chunk in enumerate(db_gap_file_chunks):
+        db_gap_task_set.append(
+            create_python_task(
+                dag,
+                f"db_gap_annotate-{index}",
+                DugUtil.annotate_db_gap_files,
+                {"files": chunk}
+            )
+        )
+
+    dug_load_topmed_variables = create_python_task(dag, "annotate_topmed", DugUtil.annotate_topmed_files)
+    bridge_task = create_python_task(dag, "bridge_task_set_db_gap_files", setup_db_gab_tasks)
+    chunked_db_gap_files = Variable.get("db_gap_chucked")
+
+    intro >> [get_topmed_files, extract_db_gap_files] >> bridge_task >> \
+    [dug_load_topmed_variables, db_gap_task_set] >> make_kg_tagged
 
