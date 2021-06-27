c1510372d97ef3eca7a14fdcb26c451542d907db
YaphetKG <kebedey@renci.org>
2021-06-27 10:29:05 -0400

    fix for all array's in kgx. Replaces single quotes with backticks

--- dags/roger/core.py
+++ dags/roger/core.py
@@ -524,6 +524,25 @@ class KGXModel:
                 f.writelines(items)
                 log.info(f"wrote : {len(items)}")
 
+    def kgx_merge_dict(self, dict_1, dict_2):
+        merged = kgx_merge_dict(dict_1, dict_2)
+        for keys in merged:
+            attribute = merged[keys]
+            # When mergeing array's for bulk loading
+            # we have to make sure that items in lists
+            # don't contain single-quotes.
+            # Single quotes in array items break parsing of arrays on bulk loading
+            # downstream.
+            if isinstance(attribute, list):
+                new_attribute = []
+                for value in attribute:
+                    if isinstance(value, str):
+                        value = value.replace("'", '`')
+                    new_attribute.append(value)
+                attribute = new_attribute
+            merged[keys] = attribute
+        return merged
+
     def sort_node_types(self, node_dict):
         categories = node_dict.get('category')
         if not categories:
@@ -558,9 +577,9 @@ class KGXModel:
             merge_time = time.time()
             log.info(f"Found matching {len(nodes_in_redis)} nodes {len(edges_in_redis)} edges from redis...")
             for node_id in nodes_in_redis:
-                nodes[node_id] = kgx_merge_dict(nodes[node_id], nodes_in_redis[node_id])
+                nodes[node_id] = self.kgx_merge_dict(nodes[node_id], nodes_in_redis[node_id])
             for edge_id in edges_in_redis:
-                edges[edge_id] = kgx_merge_dict(edges[edge_id], edges_in_redis[edge_id])
+                edges[edge_id] = self.kgx_merge_dict(edges[edge_id], edges_in_redis[edge_id])
             merge_time = time.time() - merge_time
             current_metric['merge_time'] = merge_time
             write_to_redis_time = time.time()
@@ -604,30 +623,6 @@ class KGXModel:
         if self.enable_metrics:
             path = Util.metrics_path('merge_metrics.yaml')
             Util.write_object(metrics, path)
-
-    def format_keys (self, keys, schema_type : SchemaType):
-        """ Format schema keys. Make source and destination first in edges. Make
-        id first in nodes. Remove keys for fields we can't yet represent.
-        :param keys: List of keys.
-        :param schema_type: Type of schema to conform to.
-        """
-        """ Sort keys. """
-        k_list = sorted(keys)
-        if schema_type == SchemaType.PREDICATE:
-            """ Rename subject and object to src and dest """
-            k_list.remove ('subject')
-            k_list.remove ('object')
-            k_list.insert (0, 'src')
-            k_list.insert (1, 'dest')
-        elif schema_type == SchemaType.CATEGORY:
-            """ Make id the first field. Remove smiles. It causes ast parse errors. 
-            TODO: update bulk loader to ignore AST on selected fields.
-            """
-            k_list.remove ('id')
-            if 'simple_smiles' in k_list:
-                k_list.remove ('simple_smiles')
-            k_list.insert (0, 'id')
-        return k_list
 
 
 class BiolinkModel:
