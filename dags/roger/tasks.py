c72e0ed0dbd262ea0a57c0c06152a26f60329bd8
Vladimir G <vglazunov@renci.org>
2023-07-24 18:41:26 -0400

    Deprecated DummyOperator -> EmptyOperator, imports.

--- dags/roger/tasks.py
+++ dags/roger/tasks.py
@@ -1,6 +1,6 @@
 import os
 
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 
 from roger.config import config
@@ -89,7 +89,7 @@ def create_python_task (dag, name, a_callable, func_kwargs=None):
         task_id=name,
         python_callable=task_wrapper,
         op_kwargs=op_kwargs,
-        executor_config=get_executor_config (),
+        executor_config=get_executor_config(),
         dag=dag,
         provide_context=True
     )
