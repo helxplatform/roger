1696564cd6c9488c92a6544baf1c3ff42785df4a
YaphetKG <kebedey@renci.org>
2023-10-11 14:46:06 -0400

    change dag name

--- dags/annotate_simple.py
+++ dags/annotate_simple.py
@@ -9,7 +9,7 @@ from dug_helpers.dug_utils import (
     )
 from roger.tasks import default_args, create_python_task
 
-DAG_ID = 'annotate_dug'
+DAG_ID = 'annotate_simple'
 
 """ Build the workflow's tasks and DAG. """
 with DAG(
@@ -19,13 +19,6 @@ with DAG(
 ) as dag:
 
     """Build workflow tasks."""
-    intro = EmptyOperator(task_id='Intro', dag=dag)
-
-    # Unzip and get files, avoid this because
-    # 1. it takes a bit of time making the dag itself, webserver hangs
-    # 2. Every task in this dag would still need to execute this part making it redundant
-    # 3. tasks like intro would fail because they don't have the data dir mounted.
-
     make_kg_tagged = create_python_task(dag, "make_tagged_kgx", DugUtil.make_kg_tagged)
     annotate_files = create_python_task(dag, "annotate_bacpac_files",
                                                 DugUtil.annotate_bacpac_files)        
