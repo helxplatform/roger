d3c7c24341220232cc38a31050e14d8c82332dc3
YaphetKG <kebedey@renci.org>
2023-10-12 15:18:43 -0400

    fix context

--- dags/roger/tasks.py
+++ dags/roger/tasks.py
@@ -99,7 +99,7 @@ def avalon_commit_callback(context: DagContext):
     print(the_dag.id)
     print(the_task.id)
 
-def setup_input_data(**kwargs):
+def setup_input_data(context):
     print("""
         - Figures out the task name and id,
         - find its data dependencies
@@ -107,7 +107,7 @@ def setup_input_data(**kwargs):
         - put dependency data in input dir
         - if for some reason data was not found raise an execption
           """)
-    print(kwargs)
+    print(context)
 
 def create_python_task(dag, name, a_callable, func_kwargs=None):
     """ Create a python task.
