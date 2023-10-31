ef3532be8896c3bb7b1579ffeb7d10c282212cdc
braswent <braswent6@gmail.com>
2023-10-31 16:08:29 -0400

    Adding ignore for personal testing dir and modified code

--- dags/dug_helpers/dug_utils.py
+++ dags/dug_helpers/dug_utils.py
@@ -388,13 +388,6 @@ class Dug:
             if response['total_items'] == 0:
                 log.error(f"No search elements returned for variable search: {self.variables_index}.")
                 log.error(f"Concept id : {curie}, Search term: {search_term}")
-                # raise Exception(f"Validation error - Did not find {curie} for"
-                #                 f"Search term: {search_term}")
-        else:
-            if 'total_items' in response:
-            if response['total_items'] == 0:
-                log.error(f"No search elements returned for variable search: {self.variables_index}.")
-                log.error(f"Concept id : {curie}, Search term: {search_term}")
                 raise Exception(f"Validation error - Did not find {curie} for"
                                 f"Search term: {search_term}")
             else:
