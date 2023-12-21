ba2a7d8aaf03ebe26b0eb1c3f144d8720bc0f66b
YaphetKG <kebedey@renci.org>
2023-12-21 11:07:58 -0500

    adding other bdc pipelines

---
+++ dags/roger/pipelines/kfdrc.py
@@ -0,0 +1,19 @@
+"Pipeline for BACPAC data"
+
+from roger.pipelines import DugPipeline
+from roger.core import storage
+
+class kfdrcPipeline(DugPipeline):
+    "Pipeline for BACPAC data set"
+    pipeline_name = "kfdrc"
+    parser_name = "kfdrc"
+
+    def get_objects(self, input_data_path=None):
+        if not input_data_path:
+            input_data_path = storage.dug_kfdrc_path()
+        files = storage.get_files_recursive(
+            lambda file_name: (
+                not file_name.startswith('GapExchange_')
+                and file_name.endswith('.xml')),
+            input_data_path)
+        return sorted([str(f) for f in files])
---
+++ dags/roger/pipelines/topmed.py
@@ -0,0 +1,16 @@
+"Pipeline for BACPAC data"
+
+from roger.pipelines import DugPipeline
+from roger.core import storage
+
+
+class kfdrcPipeline(DugPipeline):
+    "Pipeline for BACPAC data set"
+    pipeline_name = "topmed"
+    parser_name = "TOPMedTag"
+
+    def get_objects(self, input_data_path=None):
+        if not input_data_path:
+            input_data_path = str(storage.dug_input_files_path('topmed'))
+        topmed_file_pattern = storage.os.path.join(input_data_path, "topmed_*.csv")
+        return sorted(storage.glob.glob(topmed_file_pattern))
