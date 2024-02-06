6303ac00ddc0c51526eb55c54bad284457a1f067
YaphetKG <kebedey@renci.org>
2024-02-06 09:01:41 -0500

    fix topmed file read

--- dags/roger/pipelines/topmed.py
+++ dags/roger/pipelines/topmed.py
@@ -2,7 +2,7 @@
 
 from roger.pipelines import DugPipeline
 from roger.core import storage
-
+from roger.logger import logger
 class TopmedPipeline(DugPipeline):
     "Pipeline for Topmed data set"
     pipeline_name = "topmed"
@@ -11,6 +11,7 @@ class TopmedPipeline(DugPipeline):
     def get_objects(self, input_data_path=None):
         if not input_data_path:
             input_data_path = str(storage.dug_input_files_path('topmed'))
-        topmed_file_pattern = storage.os.path.join(input_data_path,
-                                                   "topmed_*.csv")
-        return sorted(storage.glob.glob(topmed_file_pattern))
+        files =storage.get_files_recursive(
+                lambda file_name: file_name.endswith('topmed_*.csv'),
+                input_data_path)
+        return sorted(files)
