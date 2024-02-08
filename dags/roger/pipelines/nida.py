6262a5cc54f0db81836c1bd8867f4c43bac9c157
YaphetKG <kebedey@renci.org>
2024-02-08 10:53:24 -0500

    get files pattern recursive

--- dags/roger/pipelines/nida.py
+++ dags/roger/pipelines/nida.py
@@ -14,5 +14,5 @@ class NIDAPipeline(DugPipeline):
         if not input_data_path:
             input_data_path = storage.dug_input_files_path(
                 self.get_files_dir())
-        nida_file_pattern = storage.os.path.join(input_data_path, 'NIDA-*.xml')
-        return sorted(storage.glob.glob(nida_file_pattern))
+        files = sorted(storage.get_files_recursive(lambda x: 'NIDA-' in x , input_data_path))
+        return files
