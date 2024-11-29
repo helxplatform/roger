ac8ebc2bd54c81f35de60302fe2afda65d17d283
YSK <yskale@users.noreply.github.com>
2024-11-29 12:26:30 -0500

    changed the pipeline name

--- dags/roger/pipelines/picsure_test.py
+++ dags/roger/pipelines/picsure_test.py
@@ -2,5 +2,5 @@ from roger.pipelines import DugPipeline
 
 class PicSure(DugPipeline):
     "Pipeline for BACPAC data set"
-    pipeline_name = "bdc-test5"  #lakefs 
-    parser_name = "dbgap"
+    pipeline_name = "topmedtest"  #lakefs 
+    parser_name = "dbgap_parser"
