6b93868c996c154cbd83e24181301058894079db
YSK <yskale@users.noreply.github.com>
2024-11-29 12:44:05 -0500

    parser name updated

--- dags/roger/pipelines/picsure_test.py
+++ dags/roger/pipelines/picsure_test.py
@@ -3,4 +3,4 @@ from roger.pipelines import DugPipeline
 class PicSure(DugPipeline):
     "Pipeline for BACPAC data set"
     pipeline_name = "topmedtest"  #lakefs 
-    parser_name = "dbgap_parser"
+    parser_name = "dbgap"
