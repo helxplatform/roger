86bd0375d912061ef35b357d1508fb9c39942d10
YSK <yskale@users.noreply.github.com>
2024-12-02 14:45:49 -0500

    update the get_object/files

--- dags/roger/pipelines/picsure_test.py
+++ dags/roger/pipelines/picsure_test.py
@@ -1,6 +1,26 @@
 from roger.pipelines import DugPipeline
+from roger.core import  storage
+from roger.logger import logger
+
 
 class PicSure(DugPipeline):
     "Pipeline for BACPAC data set"
     pipeline_name = "bdc-test5"  #lakefs 
     parser_name = "dbgap"
+
+    def get_objects(self, input_data_path=None):
+        """Retrieve anvil objects
+
+        This code is imported from roger.core.storage.dug_anvil_objects
+        """
+        if not input_data_path:
+            input_data_path = storage.dug_input_files_path(
+                self.files_dir)
+        files = storage.get_files_recursive(
+            lambda file_name: (
+                not file_name.startswith('GapExchange_')
+                and file_name.endswith('.xml')),
+            input_data_path)
+        logger.info("**********")
+        logger.info(files)
+        return sorted([str(f) for f in files])
