5794dc16f61f4f1de424b3bfddb51e8618e96cb4
YaphetKG <45075777+YaphetKG@users.noreply.github.com>
2025-02-18 11:42:37 -0500

    modify radx pipeline (#111)
    * modify radx pipeline
    
    * pin to dug release

--- dags/roger/pipelines/radx.py
+++ dags/roger/pipelines/radx.py
@@ -1,8 +1,18 @@
 "Pipeline for BACPAC data"
 
 from roger.pipelines import DugPipeline
+from roger.core import storage
+
 
 class RadxPipeline(DugPipeline):
-    "Pipeline for BACPAC data set"
+    "Pipeline for Radx data set"
     pipeline_name = "radx"
     parser_name = "radx"
+
+    def get_objects(self, input_data_path=None):
+        if not input_data_path:
+            input_data_path = storage.dug_kfdrc_path()
+        files = storage.get_files_recursive(
+            lambda file_name: file_name.endswith('.json'),
+            input_data_path)
+        return sorted([str(f) for f in files])
