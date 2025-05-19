6545e4a42274bf2774301606fc852d26c0858902
YaphetKG <45075777+YaphetKG@users.noreply.github.com>
2025-05-19 14:18:17 -0400

    remove clear index (#119)

--- dags/roger/pipelines/base.py
+++ dags/roger/pipelines/base.py
@@ -972,8 +972,8 @@ class DugPipeline():
         "Index concepts from expanded concept files"
         # These are concepts that have knowledge graphs  from tranql
         # clear out concepts and kg indicies from previous runs
-        self.clear_concepts_index()
-        self.clear_kg_index()
+        # self.clear_concepts_index()
+        # self.clear_kg_index()
         expanded_concepts_files = storage.dug_expanded_concept_objects(
             input_data_path, format="txt")
         for file_ in expanded_concepts_files:
