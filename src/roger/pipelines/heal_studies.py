2474b7139bfcce5ca568b95db41069b970e584e7
Michael T. Bacon <mbacon@renci.org>
2026-01-07 23:23:44 -0500

    Setuptools and python library adjustments

--- src/roger/pipelines/heal_studies.py
+++ src/roger/pipelines/heal_studies.py
@@ -11,6 +11,7 @@ class HealStudiesPipeline(DugPipeline):
     def get_objects(self, input_data_path=None):
         if not input_data_path:
             input_data_path = storage.dug_heal_study_path()
-        files = storage.get_files_recursive(lambda file_name: file_name.endswith('.xml'),
-                                    input_data_path)
+        files = storage.get_files_recursive(
+            lambda file_name: file_name.endswith('.xml'),
+            input_data_path)
         return sorted([str(f) for f in files])
