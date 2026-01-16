d23dea3b90b438c00e1eaf0647892cc41dbe1822
Michael T. Bacon <mbacon@renci.org>
2026-01-16 14:55:36 -0500

    Fix to make subsubclasses of DugPipeline work as pipelines

--- src/roger/pipelines/__init__.py
+++ src/roger/pipelines/__init__.py
@@ -4,9 +4,16 @@ import pkgutil
 from pathlib import Path
 import importlib
 
-from .base import DugPipeline
+from .base import DugPipeline, DDM2Pipeline
 
-def get_pipeline_classes(pipeline_names_dict):
+def get_all_subclasses(cls):
+    """Recurse to get all subsubclasses, etc."""
+    rval = [cls]
+    for sc in cls.__subclasses__():
+        rval.extend(get_all_subclasses(sc))
+    return rval
+
+def get_pipeline_classes(pipeline_names):
     """Return a list of all defined pipeline classes
     """
 
@@ -21,8 +28,14 @@ def get_pipeline_classes(pipeline_names_dict):
         importlib.import_module(f"{__name__}.{mod_name}")
     pipeline_list = []
 
-    for subclass in DugPipeline.__subclasses__():
-        if getattr(subclass, 'pipeline_name') and getattr(subclass, 'pipeline_name') in pipeline_names_dict.keys():
-            subclass.input_version = pipeline_names_dict[getattr(subclass, 'pipeline_name')]
+    for subclass in get_all_subclasses(DugPipeline):
+        if (getattr(subclass, 'pipeline_name') and
+            getattr(subclass, 'pipeline_name') in pipeline_names):
+            try:
+                subclass.input_version = pipeline_names[
+                    getattr(subclass, 'pipeline_name')]
+            except TypeError:
+                # If someone passed in the list, don't bother with the verison.
+                pass
             pipeline_list.append(subclass)
     return pipeline_list
