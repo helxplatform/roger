8c2cd51bb6c8747933c67916e2b421f93268b8b7
ykale <yskale@users.noreply.github.com>
2026-07-09 11:22:38 -0400

    Reduce annotation_workers default to 2 to avoid OOMKill

--- src/roger/config/__init__.py
+++ src/roger/config/__init__.py
@@ -95,7 +95,7 @@ class BulkLoaderConfig(DictLike):
 @dataclass
 class AnnotationConfig(DictLike):
     annotator_type: str = "monarch"
-    annotation_workers: int = 4
+    annotation_workers: int = 2
     annotator_args: dict = field(
         default_factory=lambda: {
             "monarch": {
